e40a405c2030009de4ec956fad6ade659e98c030
Abhay Singh <itsabhaysingh63@gmail.com>
2025-08-12 14:25:45 +0530

    Create Power of Two.py

---
+++ Power of Two.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+       if n == 1:
+        return True
+    
+       if n <= 0:
+        return False
+
+       while n%2==0:
+        n //=2
+
+       return n == 1
+
