ef309b5c4dbb20cad422b7816b0bd116fffe2ca7
Abhay Singh <itsabhaysingh63@gmail.com>
2025-08-21 15:01:05 +0530

    Update Fibonacci Number.py

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -1,7 +1,9 @@
-class Solution(object):
-    def fib(self, n):
-        if n == 1 or n == 0:
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
             return n 
+        a,b = 0,1
+        for i in range(2,n+1):
+            a,b = b,a+b
 
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        return b  
